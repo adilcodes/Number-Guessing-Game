90b2aec73e85ebedad1de40a1870fa0dcac38443
Adil <ascodesofficial@gmail.com>
2021-11-16 04:22:27 +0500

    Game's Logic has been made and its working. Just need some extra edits

--- Main.c
+++ Main.c
@@ -3,9 +3,35 @@
 #include<time.h>
 
 int main(){
-    int number;
+    int number, guess, allguesses = 1;
     srand(time(0));
     number = rand()%100 + 1; // Will Generate a random number between 1 and 100...
-    printf("The Random Number is %d", number);
+    printf("The Random Number is %d \n", number);
+
+    // Starting a loop which keeps on running until the numberis guessed
+    printf("You have only 5 attempts. \n");
+    printf("Guess the Number between 1 to 100: \n");
+    do
+    {
+            if (allguesses<6)
+            {
+            scanf("%d", &guess);
+                if (guess>number)
+                    {
+                        printf("You entered the higher number. Lower Number please: \n");
+                    }
+                    else if(guess<number){
+                        printf("You entered the lower number. Higher Number please: \n");
+                    }
+                    else{
+                        printf("Exactly!!! You won \n");
+                        printf("You guessed it in %d attempts \n", allguesses);
+                    }
+            }
+            else{
+                printf("Game Over!");
+            }
+        allguesses++;
+    } while (guess!=number && allguesses<7);
     return 0;
 }
