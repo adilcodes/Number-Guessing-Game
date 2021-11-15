a69e567a2c81e3ceac251e1c2d9233b654fc04dd
Adil <ascodesofficial@gmail.com>
2021-11-16 01:07:26 +0500

    Random Number generation Completed

--- Main.c
+++ Main.c
@@ -3,5 +3,9 @@
 #include<time.h>
 
 int main(){
-    
+    int number;
+    srand(time(0));
+    number = rand()%100 + 1; // Will Generate a random number between 1 and 100...
+    printf("The Random Number is %d", number);
+    return 0;
 }
